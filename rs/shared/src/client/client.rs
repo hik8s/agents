fecb564edac498a98fe399a63a45b15da9c0d2d4
“Martin <martin.koenig@ymail.com>
2024-11-15 10:46:30 +0100

    feat(shared,hik8s-client): add retry middleware

--- rs/shared/src/client/client.rs
+++ rs/shared/src/client/client.rs
@@ -1,21 +1,28 @@
 use crate::env::get_env_var;
 use reqwest::header::AUTHORIZATION;
 use reqwest::{multipart::Form, Client};
+use serde::Serialize;
 
 use super::auth::Auth;
 use super::Hik8sClientError;
 
 #[derive(Clone)]
 pub struct Hik8sClient {
-    pub client: Client,
+    pub client: ClientWithMiddleware,
     pub host: String,
     auth: Auth,
 }
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
 
 impl Hik8sClient {
     pub fn new() -> Result<Self, Hik8sClientError> {
         let host = get_env_var("HIK8S_HOST")?;
+        let retry_policy = ExponentialBackoff::builder().build_with_max_retries(10);
         let client = Client::builder().use_rustls_tls().build()?;
+        let client = ClientBuilder::new(client)
+            .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+            .build();
         let auth = Auth::new()?;
         Ok(Self { client, host, auth })
     }
@@ -53,5 +60,14 @@ impl Hik8sClient {
             .await?
             .error_for_status()?;
         Ok(())
+    }
+
+    pub async fn dispatch<T: Serialize>(
+        &self,
+        resource: T,
+        route: &str,
+    ) -> Result<(), Hik8sClientError> {
+        let resource_json = serde_json::to_value(resource)?;
+        self.send_request(route, &resource_json).await
     }
 }
--- rs/shared/src/client/error.rs
+++ rs/shared/src/client/error.rs
@@ -8,8 +8,12 @@ use super::auth::AuthError;
 pub enum Hik8sClientError {
     #[error("Reqwest error: {0}")]
     ReqwestError(#[from] reqwest::Error),
+    #[error("Reqwest middleware error: {0}")]
+    ReqwestMiddlewareError(#[from] reqwest_middleware::Error),
     #[error("Environment variable error: {0}")]
     EnvError(#[from] EnvError),
     #[error("Auth error: {0}")]
     AuthError(#[from] AuthError),
+    #[error("Json serialize error: {0}")]
+    JsonError(#[from] serde_json::Error),
 }
